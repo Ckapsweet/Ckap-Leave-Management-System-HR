7dc35bd9841b50c4f296702b6abda9cc1938cebe
bnwDunk <teerapong002588@gmail.com>
2026-03-30 09:09:22 +0700

    edit error econn

--- server.js
+++ server.js
@@ -3,4 +3,4 @@ import "dotenv/config";
 import app from "./app.js";
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, "0.0.0.0", () => console.log(`Server running on http://0.0.0.0:${PORT}`));
