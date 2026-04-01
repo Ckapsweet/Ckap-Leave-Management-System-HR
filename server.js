6c22227a0cd3a9febc415b01085f2e25769e6378
bnwDunk <teerapong002588@gmail.com>
2026-04-01 14:57:18 +0700

    test auto cicd

--- server.js
+++ server.js
@@ -2,4 +2,4 @@ import "dotenv/config";
 import app from "./app.js";
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, "0.0.0.0", () => console.log(`Server running running running on http://0.0.0.0:${PORT}`));
+app.listen(PORT, "0.0.0.0", () => console.log(`Server running on http://0.0.0.0:${PORT}`));
