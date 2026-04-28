090960a0b7992c18e78bd57e41e0215c14809e0d
bnwDunk <teerapong002588@gmail.com>
2026-04-28 10:25:57 +0700

    edit data server

--- migrate_departments.js
+++ migrate_departments.js
@@ -1,11 +1,14 @@
 import mysql from "mysql2/promise";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 async function migrate() {
   const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'ckap_leave_sys'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
   });
 
   try {
